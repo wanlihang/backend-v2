54aa2c8869cef0f19c2d5885a5fedd90290eca67
xtt <xx@xx.com>
2021-07-13 16:05:42 +0800

    封装选择图片按钮组件

--- src/views/test/test.vue
+++ src/views/test/test.vue
@@ -2,18 +2,31 @@
   <div>
     <!-- wangEditor 用法 -->
     <wang-editor html="123" @change="change"></wang-editor>
+
+    <div>
+      <upload-image
+        v-model="img"
+        helper="图片1200x800"
+        :height="150"
+        :width="200"
+      ></upload-image>
+    </div>
   </div>
 </template>
 
 <script>
 import WangEditor from "@/components/wangeditor";
+import UploadImage from "@/components/upload-image";
 
 export default {
   components: {
     WangEditor,
+    UploadImage,
   },
   data() {
-    return {};
+    return {
+      img: 'http://public.qiniu.1o1.cc/images/dKeiIHRWoAnqcJRPdn5FrxjToO7E1Hl9tsyWE47Z.jpg',
+    };
   },
   methods: {
     change(v) {
