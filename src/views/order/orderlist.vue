05b22b9d54c8edb4d2e310cbc54cf07847e8b618
禺狨 <18119604035@163.com>
2021-07-13 18:47:54 +0800

    列表页初始化

--- src/views/order/orderlist.vue
+++ src/views/order/orderlist.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>123</div>
+  <el-container>
+    <!-- <el-header>
+      <h1>订单</h1>
+    </el-header> -->
+    <el-main class="main_content">
+      <div class="row"></div>
+      <el-table :data="dataList" stripe style="width: 100%">
+        <el-table-column prop="id" label="ID"> </el-table-column>
+        <el-table-column prop="user_id" label="用户ID"> </el-table-column>
+        <el-table-column prop="nick_name" label="用户"> </el-table-column>
+        <el-table-column prop="order_id" label="订单号"> </el-table-column>
+        <el-table-column prop="charge" label="总价"> </el-table-column>
+        <el-table-column prop="status_text" label="状态"> </el-table-column>
+        <!-- <el-table-column prop="" label="图片">
+          <template slot-scope="scope">
+            <img :src="scope.row.title_img" alt="" style="width: 60px" />
+          </template>
+        </el-table-column> -->
+        <el-table-column prop="goods" label="商品"> </el-table-column>
+        <el-table-column prop="updated_at" label="时间"> </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              type="danger"
+              @click="changeData(scope.row.id, scope.$index)"
+              >改为已支付</el-button
+            >
+            <el-button size="mini" type="primary" @click="allAdd(scope.row)"
+              >详情</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
@@ -9,6 +44,10 @@ export default {
       name: "Orderlist",
       page: 1,
       loading: false,
+      user_id: "",
+      status: "",
+      order_id: "",
+      dataList: [],
     };
   },
   created() {
@@ -28,12 +67,43 @@ export default {
       };
       this.$api.Order.List(data).then((resp) => {
         if (resp.status == 0) {
-          this.page = resp.data.current_page + 1;
-          console.log(111);
+          this.page = resp.data.orders.current_page + 1;
+          var orders = resp.data.orders;
+          var users = resp.data.users;
+          this.dataList = orders.data;
+          for (var i = 0; i < this.dataList.length; i++) {
+            var showkey = this.dataList[i].user_id;
+            this.parseJson(users, showkey);
+          }
         }
         this.loading = false;
       });
     },
+    //搜索box
+    parseJson(data, index) {
+      if (typeof data[index] === "undefined") {
+        //console.log("data无值");
+        return "";
+      }
+      for (var i = 0; i < this.dataList.length; i++) {
+        if (this.dataList[i].user_id == index) {
+          this.dataList[i]["nick_name"] = data[index].nick_name;
+        }
+      }
+    },
   },
 };
 </script>
+<style  lang="less" >
+.row {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  padding-left: 20px;
+  box-sizing: border-box;
+}
+.el-main {
+  width: 100%;
+  padding: 5px;
+}
+</style>
