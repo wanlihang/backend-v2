fd15a67bf6888f4678e53b84eeaeda34c33ec023
禺狨 <18119604035@163.com>
2021-07-21 09:39:43 +0800

    分销商品全部

--- src/views/multi_level_share/goods/update.vue
+++ src/views/multi_level_share/goods/update.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="meedu-main-body">
-    <back-bar class="mb-30" title="创建分销商品"></back-bar>
+    <back-bar class="mb-30" title="编辑分销商品"></back-bar>
     <div class="float-left">
       <div class="form-box broder-top-left-radius">
         <el-form ref="form" :model="course" :rules="rules" label-width="200px">
