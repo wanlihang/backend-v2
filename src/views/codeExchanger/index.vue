383d451e5923d009d96c5e5aeb10a9832b9a3a5b
禺狨 <18119604035@163.com>
2021-07-22 10:09:48 +0800

    优化

--- src/views/codeExchanger/index.vue
+++ src/views/codeExchanger/index.vue
@@ -53,8 +53,7 @@
           <el-table-column prop="goods_title" label="商品"> </el-table-column>
           <el-table-column label="价格" width="120">
             <template slot-scope="scope">
-              <span
-                >{{ scope.row.goods_charge}}元</span>
+              <span>{{ scope.row.goods_charge }}元</span>
             </template>
           </el-table-column>
           <el-table-column fixed="right" label="操作" width="150">
@@ -179,7 +178,7 @@ export default {
             })
             .catch((e) => {
               this.loading = false;
-              this.$message(e.message);
+              this.$message.error(e.message);
             });
         })
         .catch(() => {
@@ -190,32 +189,3 @@ export default {
 };
 </script>
 
-<style lang="less" scoped>
-.filter-box {
-  width: 100%;
-  height: auto;
-  float: left;
-  box-sizing: border-box;
-  padding: 30px;
-  border-radius: 15px;
-  margin-bottom: 15px;
-  background-color: white;
-
-  .filter-label {
-    font-size: 14px;
-    color: rgba(0, 0, 0, 0.7);
-  }
-}
-.user-item {
-  width: auto;
-  display: flex;
-  align-items: center;
-  .avatar {
-    margin-right: 10px;
-  }
-  .nickname {
-    font-size: 15px;
-    font-weight: normal;
-  }
-}
-</style>
