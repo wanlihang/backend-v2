d7ddc6f8da26b912c624f12fd7f70a0e71373173
xtt <xx@xx.com>
2021-07-23 10:36:23 +0800

    优惠码优化

--- src/views/order/create_code.vue
+++ src/views/order/create_code.vue
@@ -1,49 +1,80 @@
 <template>
   <div class="meedu-main-body">
     <back-bar class="mb-30" title="创建优惠码"></back-bar>
+
     <div class="float-left">
       <el-form
         ref="form"
         class="float-left"
-        :model="course"
+        :model="form"
         :rules="rules"
         label-width="200px"
       >
         <el-form-item label="优惠码" prop="code">
-          <el-input
-            class="w-200px"
-            v-model="course.code"
-            placeholder="请勿使用U或者u开头"
-          ></el-input>
-        </el-form-item>
-        <el-form-item label="到期时间" prop="expired_at">
-          <div>
-            <el-date-picker
-              v-model="course.expired_at"
-              type="datetime"
-              format="yyyy-MM-dd HH:mm"
-              value-format="yyyy-MM-dd HH:mm"
-              placeholder="请选择日期"
-              :picker-options="expireTimeOption"
-            >
-            </el-date-picker>
+          <div class="d-flex">
+            <div>
+              <el-input
+                class="w-200px"
+                v-model="form.code"
+                placeholder="优惠码"
+              ></el-input>
+            </div>
+            <div class="ml-10">
+              <helper-text text="请勿使用大写U或者小写u开头"></helper-text>
+            </div>
           </div>
         </el-form-item>
-        <el-form-item label="抵扣金额" prop="invited_user_reward">
-          <el-input
-            class="w-200px"
-            type="number"
-            v-model="course.invited_user_reward"
-            placeholder="要求：整数。单位：元"
-          ></el-input>
+        <el-form-item label="到期时间" prop="expired_at">
+          <div class="d-flex">
+            <div>
+              <el-date-picker
+                v-model="form.expired_at"
+                type="datetime"
+                format="yyyy-MM-dd HH:mm"
+                value-format="yyyy-MM-dd HH:mm"
+                placeholder="请选择日期"
+                :picker-options="expireTimeOption"
+              >
+              </el-date-picker>
+            </div>
+            <div class="ml-10">
+              <helper-text
+                text="过期时间到了之后优惠码便无法使用了。"
+              ></helper-text>
+            </div>
+          </div>
+        </el-form-item>
+        <el-form-item label="面值" prop="invited_user_reward">
+          <div class="d-flex">
+            <div>
+              <el-input
+                class="w-200px"
+                type="number"
+                v-model="form.invited_user_reward"
+                placeholder="面值"
+              ></el-input>
+            </div>
+            <div class="ml-10">
+              <helper-text
+                text="请输入整数。不支持小数。可在收银台抵扣的金额。"
+              ></helper-text>
+            </div>
+          </div>
         </el-form-item>
         <el-form-item label="可使用次数" prop="use_times">
-          <el-input
-            class="w-200px"
-            type="number"
-            v-model="course.use_times"
-            placeholder="0=不限制"
-          ></el-input>
+          <div class="d-flex">
+            <div>
+              <el-input
+                class="w-200px"
+                type="number"
+                v-model="form.use_times"
+                placeholder="可使用次数"
+              ></el-input>
+            </div>
+            <div class="ml-10">
+              <helper-text text="请输入整数。0意味着不限制。"></helper-text>
+            </div>
+          </div>
         </el-form-item>
       </el-form>
       <div class="bottom-menus">
@@ -64,11 +95,10 @@
 
 <script>
 export default {
-  name: "Create_code",
   data() {
     return {
       loading: false,
-      course: {
+      form: {
         code: null,
         expired_at: null,
         invited_user_reward: null,
@@ -126,7 +156,7 @@ export default {
         return;
       }
       this.loading = true;
-      this.$api.Order.PromoCode.Create(this.course)
+      this.$api.Order.PromoCode.Create(this.form)
         .then(() => {
           this.$message.success(this.$t("common.success"));
           this.$router.back();
--- src/views/order/createmulti_code.vue
+++ src/views/order/createmulti_code.vue
@@ -1,41 +1,78 @@
 <template>
   <div class="meedu-main-body">
     <back-bar class="mb-30" title="批量生成优惠码"></back-bar>
+
     <div class="float-left">
       <el-form
         ref="form"
         class="float-left"
-        :model="course"
+        :model="form"
         :rules="rules"
         label-width="200px"
       >
         <el-form-item label="统一前缀" prop="prefix">
-          <el-input class="w-200px" v-model="course.prefix"></el-input>
+          <el-input
+            class="w-200px"
+            placeholder="前缀"
+            v-model="form.prefix"
+          ></el-input>
         </el-form-item>
+
         <el-form-item label="生成数量" prop="num">
-          <el-input
-            class="w-200px"
-            type="number"
-            v-model="course.num"
-          ></el-input>
+          <div class="d-flex">
+            <div>
+              <el-input
+                class="w-200px"
+                type="number"
+                v-model="form.num"
+                placeholder="生成数量"
+              ></el-input>
+            </div>
+            <div class="ml-10">
+              <helper-text
+                text="请输入整数。为防止系统卡顿导致生成失败，请勿输入超过1000的数字。"
+              ></helper-text>
+            </div>
+          </div>
         </el-form-item>
+
         <el-form-item label="到期时间" prop="expired_at">
-          <el-date-picker
-            v-model="course.expired_at"
-            type="datetime"
-            format="yyyy-MM-dd HH:mm"
-            value-format="yyyy-MM-dd HH:mm"
-            placeholder="请选择日期"
-            :picker-options="expireTimeOption"
-          >
-          </el-date-picker>
+          <div class="d-flex">
+            <div>
+              <el-date-picker
+                v-model="form.expired_at"
+                type="datetime"
+                format="yyyy-MM-dd HH:mm"
+                value-format="yyyy-MM-dd HH:mm"
+                placeholder="请选择日期"
+                :picker-options="expireTimeOption"
+              >
+              </el-date-picker>
+            </div>
+            <div class="ml-10">
+              <helper-text
+                text="该字段决定优惠码的有效期限，到了选定的时间就无法使用了。"
+              ></helper-text>
+            </div>
+          </div>
         </el-form-item>
+
         <el-form-item label="面值" prop="money">
-          <el-input
-            class="w-200px"
-            type="number"
-            v-model="course.money"
-          ></el-input>
+          <div class="d-flex">
+            <div>
+              <el-input
+                class="w-200px"
+                type="number"
+                v-model="form.money"
+                placeholder="面值"
+              ></el-input>
+            </div>
+            <div class="ml-10">
+              <helper-text
+                text="请输入整数。不支持小数。面值是用户使用该码在收银台可抵扣的金额。"
+              ></helper-text>
+            </div>
+          </div>
         </el-form-item>
       </el-form>
       <div class="bottom-menus">
@@ -60,7 +97,7 @@ export default {
   data() {
     return {
       loading: false,
-      course: {
+      form: {
         prefix: "",
         expired_at: "",
         money: "",
@@ -118,7 +155,7 @@ export default {
         return;
       }
       this.loading = true;
-      this.$api.Order.PromoCode.CreateMulti(this.course)
+      this.$api.Order.PromoCode.CreateMulti(this.form)
         .then(() => {
           this.$message.success(this.$t("common.success"));
           this.$router.back();
