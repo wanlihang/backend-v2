e55c9376f615da0fd44bebfc85fc904e1d2db59b
xtt <xx@xx.com>
2021-07-23 11:39:29 +0800

    目录调整

--- src/js/menus.js
+++ src/js/menus.js
@@ -1,160 +1,160 @@
 export default [{
-        name: 'title.dashboard',
-        key: 'Dashboard',
-        children: [],
-        icon: 'icon-icon-study-n',
+    name: 'title.dashboard',
+    key: 'Dashboard',
+    children: [],
+    icon: 'icon-icon-study-n',
+},
+{
+    name: 'title.coursename',
+    key: 'Course',
+    icon: 'icon-icon-lesson',
+    children: [{
+        name: 'title.topic.name',
+        key: 'Topic'
     },
     {
-        name: 'title.coursename',
-        key: 'Course',
-        icon: 'icon-icon-lesson',
-        children: [{
-                name: 'title.topic.name',
-                key: 'Topic'
-            },
-            {
-                name: 'title.course.vod.index',
-                key: 'Vod'
-            },
-            {
-                name: 'title.live.name',
-                key: 'LiveCourse'
-            },
-            {
-                name: 'title.meedubook.book.index',
-                key: 'Meedubook'
-            },
-            {
-                name: 'title.learningpath.path.name',
-                key: 'LearningPath'
-            },
-        ]
+        name: 'title.course.vod.index',
+        key: 'Vod'
     },
     {
+        name: 'title.live.name',
+        key: 'LiveCourse'
+    },
+    {
+        name: 'title.meedubook.book.index',
+        key: 'Meedubook'
+    },
+    {
+        name: 'title.learningpath.path.name',
+        key: 'LearningPath'
+    },
+    ]
+},
+{
+    name: 'title.exam.name',
+    key: 'Exam',
+    icon: 'icon-icon-exam-n',
+    children: [{
+        name: 'title.exam.paper.index',
+        key: 'ExamPaper',
+    },
+    {
+        name: 'title.exam.practice.index',
+        key: 'ExamPractice',
+    }
+    ]
+},
+{
+    name: 'title.member.index',
+    key: 'Member',
+    icon: 'icon-icon-me-n',
+    children: [{
         name: 'title.member.index',
-        key: 'Member',
-        icon: 'icon-icon-me-n',
-        children: [{
-            name: 'title.member.index',
-            key: 'MemberIndex',
-        }]
-    },
-    {
-        name: 'title.finance',
-        key: 'Finance',
-        icon: 'icon-icon-money-n',
-        children: [
+        key: 'MemberIndex',
+    }]
+},
+{
+    name: 'title.finance',
+    key: 'Finance',
+    icon: 'icon-icon-money-n',
+    children: [
 
-            {
-                name: 'title.order',
-                key: 'Orderlist'
-            },
-            {
-                name: 'title.miaosha.orders.index',
-                key: 'MiaoshaOrders'
-            },
-            {
-                name: 'title.tuangou.orders',
-                key: 'TuangouGoodsOrder'
-            },
-            {
-                name: 'title.withdrawOrders',
-                key: 'WithdrawOrders'
-            },
-        ]
+        {
+            name: 'title.order',
+            key: 'Orderlist'
+        },
+        {
+            name: 'title.miaosha.orders.index',
+            key: 'MiaoshaOrders'
+        },
+        {
+            name: 'title.tuangou.orders',
+            key: 'TuangouGoodsOrder'
+        },
+        {
+            name: 'title.withdrawOrders',
+            key: 'WithdrawOrders'
+        },
+    ]
+},
+{
+    name: 'title.operate',
+    key: 'Operate',
+    icon: 'icon-icon-operate',
+    children: [{
+        name: 'title.codeExchanger.index',
+        key: 'CodeExchanger'
     },
     {
-        name: 'title.operate',
-        key: 'Operate',
-        icon: 'icon-icon-operate',
-        children: [{
-                name: 'title.codeExchanger.index',
-                key: 'CodeExchanger'
-            },
-            {
-                name: 'title.promocode',
-                key: 'Promocode'
-            },
-            {
-                name: 'title.role',
-                key: 'Role'
-            },
-            {
-                name: 'title.tuangou.goods.index',
-                key: 'TuangouGoods'
-            },
-            {
-                name: 'title.miaosha.goods.index',
-                key: 'MiaoshaGoods'
-            },
-            {
-                name: 'title.multi_level_share.goods.index',
-                key: 'ShareGoods'
-            },
-            {
-                name: 'title.multi_level_share.poster.index',
-                key: 'MultiPoster'
-            },
-            {
-                name: 'title.wenda.question.index',
-                key: 'Question'
-            },
-            {
-                name: 'title.wechat.messagereply.index',
-                key: 'MessageReply'
-            },
-        ]
+        name: 'title.promocode',
+        key: 'Promocode'
     },
     {
-        name: 'title.exam.name',
-        key: 'Exam',
-        icon: 'icon-icon-exam-n',
-        children: [{
-                name: 'title.exam.paper.index',
-                key: 'ExamPaper',
-            },
-            {
-                name: 'title.exam.practice.index',
-                key: 'ExamPractice',
-            }
-        ]
+        name: 'title.role',
+        key: 'Role'
     },
     {
-        name: 'title.decoration',
-        key: 'Decoration',
-        icon: 'icon-icon-decorate',
-        children: [{
-                name: 'title.decoration_pc',
-                key: 'DecorationPC',
-            },
-            {
-                name: 'title.decoration_h5',
-                key: 'DecorationH5'
-            },
+        name: 'title.tuangou.goods.index',
+        key: 'TuangouGoods'
+    },
+    {
+        name: 'title.miaosha.goods.index',
+        key: 'MiaoshaGoods'
+    },
+    {
+        name: 'title.multi_level_share.goods.index',
+        key: 'ShareGoods'
+    },
+    {
+        name: 'title.multi_level_share.poster.index',
+        key: 'MultiPoster'
+    },
+    {
+        name: 'title.wenda.question.index',
+        key: 'Question'
+    },
+    {
+        name: 'title.wechat.messagereply.index',
+        key: 'MessageReply'
+    },
+    ]
+},
+{
+    name: 'title.decoration',
+    key: 'Decoration',
+    icon: 'icon-icon-decorate',
+    children: [{
+        name: 'title.decoration_pc',
+        key: 'DecorationPC',
+    },
+    {
+        name: 'title.decoration_h5',
+        key: 'DecorationH5'
+    },
 
-            {
-                name: 'title.singlepage.index',
-                key: 'SinglePage'
-            },
-        ],
+    {
+        name: 'title.singlepage.index',
+        key: 'SinglePage'
+    },
+    ],
+},
+{
+    name: 'title.system',
+    key: 'System',
+    icon: 'icon-icon-setting-n',
+    children: [{
+        name: 'title.system-administrator',
+        key: 'SystemAdministrator',
     },
     {
-        name: 'title.system',
-        key: 'System',
-        icon: 'icon-icon-setting-n',
-        children: [{
-                name: 'title.system-administrator',
-                key: 'SystemAdministrator',
-            },
-            {
-                name: 'title.system-config',
-                key: 'SystemConfig',
-            },
-            {
-                name: 'title.system-application',
-                key: 'SystemApplication',
-            }
-        ]
+        name: 'title.system-config',
+        key: 'SystemConfig',
     },
+    {
+        name: 'title.system-application',
+        key: 'SystemApplication',
+    }
+    ]
+},
 
 ];
