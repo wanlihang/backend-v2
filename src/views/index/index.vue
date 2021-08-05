c5572aba46c493b4b6f4fd59695b264d574d691e
xtt <xx@xx.com>
2021-08-05 10:46:17 +0800

    update ver

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -168,7 +168,7 @@
       <p class="info">
         <span>PHP{{ systemInfo.php_version }} </span>
         <span class="mx-10">主程序{{ systemInfo.meedu_version }}</span>
-        <span>后管v4.4.4</span>
+        <span>后管v4.4.5</span>
       </p>
     </div>
   </div>
