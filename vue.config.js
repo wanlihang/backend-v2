c13dd0ec1d8d11fdbe5cdfaa05264d44f27a3586
xtt <xx@xx.com>
2021-10-10 10:41:55 +0800

    取消反向代理

--- vue.config.js
+++ vue.config.js
@@ -6,15 +6,15 @@ function resolve(dir){
 module.exports = {
     publicPath: '',
     productionSourceMap: false,
-    devServer: {
-        proxy: {
-            '/backend':{
-                target:'http://192.168.3.9:8000',
-                changeOrigin:true,
-                ws:true
-            }
-        }
-    },
+    // devServer: {
+    //     proxy: {
+    //         '/backend':{
+    //             target:'http://192.168.3.9:8000',
+    //             changeOrigin:true,
+    //             ws:true
+    //         }
+    //     }
+    // },
     pluginOptions: {
         'style-resources-loader': {
           preProcessor: 'less',
