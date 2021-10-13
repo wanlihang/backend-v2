5c3544045314c5e9b9b418b50b8ae47ac87c30a6
xtt <xx@xx.com>
2021-10-13 17:18:06 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,18 @@
 // vue.config.js
 const pathroot = require('path');
-function resolve(dir){
-    return path.join(__dirname,dir)
-}
+
 module.exports = {
     publicPath: '',
     productionSourceMap: false,
-    // devServer: {
-    //     proxy: {
-    //         '/backend':{
-    //             target:'http://192.168.3.9:8000',
-    //             changeOrigin:true,
-    //             ws:true
-    //         }
-    //     }
-    // },
+    devServer: {
+        proxy: {
+            '/backend':{
+                target:'http://192.168.3.9:8000',
+                changeOrigin:true,
+                ws:true
+            }
+        }
+    },
     pluginOptions: {
         'style-resources-loader': {
           preProcessor: 'less',
