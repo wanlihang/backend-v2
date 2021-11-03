70118469c9519fc4efa64b3441c88292897cae31
0xtyz <616896861@qq.com>
2021-11-03 10:01:03 +0800

    update version

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -177,7 +177,7 @@
       <p class="info">
         <span>PHP{{ systemInfo.php_version }} </span>
         <span class="mx-10">主程序{{ systemInfo.meedu_version }}</span>
-        <span>后管v4.4.16</span>
+        <span>后管v4.4.15</span>
       </p>
     </div>
   </div>
