381d60f47ea6cce568d381c10b5ad4b29e4518db
0xtyz <616896861@qq.com>
2021-11-09 15:39:05 +0800

    接入quill-editor编辑器

--- src/components/quill-editor.vue
+++ src/components/quill-editor.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="quill-editor-box">
-    <quill-editor
-      class="quill-editor-obj"
+    <div
       ref="myQuillEditor"
-      :value="value"
-      :options="editorOption"
-      @change="onEditorChange"
+      class="quill-editor"
       :style="{ height: (height || 'auto') + 'px' }"
-    />
+    >
+      <slot name="toolbar"></slot>
+      <div ref="editor"></div>
+    </div>
 
     <select-image
       :show="showUploadImage"
@@ -19,77 +19,93 @@
 </template>
 
 <script>
+import _Quill from "quill";
+
 import "quill/dist/quill.core.css";
 import "quill/dist/quill.snow.css";
 import "quill/dist/quill.bubble.css";
 
-import { quillEditor } from "vue-quill-editor";
 import debounce from "lodash/debounce";
 
 import SelectImage from "@/components/select-image";
 
 export default {
   components: {
-    quillEditor,
     SelectImage,
   },
   props: ["value", "height"],
   data() {
     return {
+      quill: null,
       showUploadImage: false,
       editorIndex: 0,
       editorOption: {
+        theme: "snow",
         modules: {
           toolbar: [
-            ["bold", "italic", "underline", "strike"], // toggled buttons
-
+            ["bold", "italic", "underline", "strike"],
             ["blockquote", "code-block"],
-
+            [{ header: 1 }, { header: 2 }],
             [{ list: "ordered" }, { list: "bullet" }],
-
-            [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+            [{ size: ["small", false, "large", "huge"] }],
             [{ header: [1, 2, 3, 4, 5, 6, false] }],
-
             [{ color: [] }, { background: [] }],
             [{ align: [] }],
-
-            ["link", "image"],
-
             ["clean"],
+            ["link", "image"],
           ],
         },
         placeholder: "请输入内容...",
+        readOnly: false,
       },
     };
   },
-  computed: {
-    editor() {
-      return this.$refs.myQuillEditor.quill;
-    },
-  },
   mounted() {
-    this.$nextTick(() => {
-      this.editor.getModule("toolbar").addHandler("image", () => {
+    this.init();
+  },
+  beforeDestroy() {
+    this.quill = null;
+    delete this.quill;
+  },
+  methods: {
+    init() {
+      this.quill = new _Quill(this.$refs["myQuillEditor"], this.editorOption);
+
+      // 初始值
+      this.quill.pasteHTML(this.value || "");
+
+      // 自定义imageHandler
+      this.quill.getModule("toolbar").addHandler("image", () => {
         this.showUploadImage = true;
       });
 
-      this.editor.on("selection-change", (range, oldRange) => {
+      // 监听记录Index
+      this.quill.on("selection-change", (range, oldRange) => {
         if (oldRange === null || oldRange.index === 0) {
-          this.editorIndex = this.editor.getLength();
+          this.editorIndex = this.quill.getLength();
         } else {
           this.editorIndex = oldRange.index;
         }
         console.log(this.editorIndex);
       });
-    });
-  },
-  methods: {
-    onEditorChange: debounce(function (e) {
-      console.log(e);
-      this.$emit("input", e.html);
-    }, 400),
+
+      // 值变化
+      this.quill.on(
+        "text-change",
+        debounce(() => {
+           let html = this.$refs.myQuillEditor.children[0].innerHTML
+          if (html === "<p><br></p>") {
+            html = "";
+          }
+          this.onEditorChange(html);
+        }, 400)
+      );
+    },
+    onEditorChange(val) {
+      this.$emit("input", val);
+    },
     uploadImage(imgUrl) {
-      this.editor.insertEmbed(this.editorIndex, "image", imgUrl);
+      this.quill.insertEmbed(this.editorIndex, "image", imgUrl);
       this.showUploadImage = false;
     },
   },
@@ -101,5 +117,18 @@ export default {
   width: 100%;
   height: auto;
   float: left;
+}
+</style>
+
+<style lang="less">
+.quill-editor-box {
+  .ql-picker-label::before {
+    position: absolute;
+  }
+  .ql-picker-label {
+    svg {
+      display: block;
+    }
+  }
 }
 </style>
