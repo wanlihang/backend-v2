230908e2cdfe1ff9b1b9d5ef5b39739124f2a825
禺狨 <18119604035@163.com>
2021-11-23 15:34:01 +0800

    系统配置新页面和样式优化

--- src/views/system/config.vue
+++ src/views/system/config.vue
@@ -1,91 +1,51 @@
 <template>
-  <div class="config-box" v-loading="loading">
-    <div class="options">
-      <div class="body">
-        <el-select
-          v-model="activeItem"
-          placeholder="请选择需要配置的栏目"
-          style="width: 100%"
-        >
-          <el-option
-            v-for="(item, index) in groups"
-            :key="index"
-            :label="item.name"
-            :value="item.key"
-          >
-          </el-option>
-        </el-select>
-      </div>
-      <div class="extra">
-        <el-button type="primary" @click="save">保存</el-button>
-      </div>
-    </div>
+  <div class="meedu-main-body" v-loading="loading">
+    <back-bar class="mb-30" :title="key"></back-bar>
+    <div class="float-left" v-if="config">
+      <el-form ref="form" label-width="155px">
+        <template v-for="c in config[key]">
+          <el-form-item :key="c.id" :label="c.name" >
+            <div class="d-flex" style="margin-left:3px;">
+              <div>
+                <!-- 单行文本输入框 -->
+                <el-input v-if="c.field_type === 'text'" v-model="c.value"></el-input>
+                <!-- 数字输入框 -->
+                <el-input v-else-if="c.field_type === 'number'" v-model="c.value"></el-input>
+                <!-- 多行文本输入框 -->
+                <el-input v-else-if="c.field_type === 'textarea'" type="textarea" :rows="3" v-model="c.value">
+                </el-input>
+                <!-- 富文本输入框 -->
+                <quill-editor :height="400" v-model="c.value" v-else-if="c.field_type === 'longtext'"></quill-editor>
+                <!-- 图片上传 -->
+                <upload-image v-model="c.value" :name="c.name" v-else-if="c.field_type === 'image'"></upload-image>
 
-    <div class="config-body" v-if="config">
-      <template v-for="c in config[activeItem]">
-        <div class="config-item" :key="c.id">
-          <div class="label-name">{{ c.name }}</div>
-          <div class="input-box">
-            <!-- 单行文本输入框 -->
-            <el-input
-              v-if="c.field_type === 'text'"
-              v-model="c.value"
-            ></el-input>
-            <!-- 数字输入框 -->
-            <el-input
-              v-else-if="c.field_type === 'number'"
-              v-model="c.value"
-            ></el-input>
-            <!-- 多行文本输入框 -->
-            <el-input
-              v-else-if="c.field_type === 'textarea'"
-              type="textarea"
-              :rows="3"
-              v-model="c.value"
-            >
-            </el-input>
-            <!-- 富文本输入框 -->
-            <quill-editor
-              :height="400"
-              v-model="c.value"
-              v-else-if="c.field_type === 'longtext'"
-            ></quill-editor>
-            <!-- 图片上传 -->
-            <upload-image
-              v-model="c.value"
-              :name="c.name"
-              v-else-if="c.field_type === 'image'"
-            ></upload-image>
+                <!-- 开关 -->
+                <el-switch v-else-if="c.field_type === 'switch'" v-model="c.value" active-value="1" inactive-value="0">
+                </el-switch>
 
-            <!-- 开关 -->
-            <el-switch
-              v-else-if="c.field_type === 'switch'"
-              v-model="c.value"
-              active-value="1"
-              inactive-value="0"
-            >
-            </el-switch>
-
-            <!-- 选择 -->
-            <el-select
-              class="w-300px"
-              v-model="c.value"
-              v-else-if="c.field_type === 'select'"
-            >
-              <el-option
-                v-for="(item, index) in c.option_value"
-                :key="index"
-                :label="item.title"
-                :value="item.key"
-              >
-              </el-option>
-            </el-select>
+                <!-- 选择 -->
+                <el-select class="w-300px" v-model="c.value" v-else-if="c.field_type === 'select'">
+                  <el-option v-for="(item, index) in c.option_value" :key="index" :label="item.title" :value="item.key">
+                  </el-option>
+                </el-select>
+              </div>
+              <div class="ml-10" v-if="c.help">
+                {{ c.help }}
+              </div>
+            </div>
+          </el-form-item>
+        </template>
+      </el-form>
+      <div class="bottom-menus">
+        <div class="bottom-menus-box">
+          <div>
+            <el-button @click="save" :loading="loading" type="primary">保存</el-button>
           </div>
-          <div class="helper-box" v-if="c.help">
-            {{ c.help }}
+          <div class="ml-24">
+            <el-button @click="$router.back()">取消</el-button>
           </div>
         </div>
-      </template>
+      </div>
     </div>
   </div>
 </template>
@@ -101,25 +61,10 @@ export default {
   },
   data() {
     return {
-      activeItem: "系统",
+      key: this.$route.query.key,
       config: null,
       loading: false,
     };
-  },
-  computed: {
-    groups() {
-      if (!this.config) {
-        return [];
-      }
-      let groups = [];
-      for (var i in this.config) {
-        groups.push({
-          name: i,
-          key: i,
-        });
-      }
-      return groups;
-    },
   },
   mounted() {
     this.getConfig();
@@ -178,76 +123,8 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.config-box {
+.meedu-main-body {
   width: 100%;
-  height: auto;
-  float: left;
-  border: 1px solid rgb(227, 227, 227);
-  border-radius: 3px;
-
-  .options {
-    display: flex;
-    align-items: center;
-    width: 100%;
-    height: auto;
-    float: left;
-    box-sizing: border-box;
-    padding: 15px;
-    background-color: rgb(243, 245, 247);
-    color: #333;
-
-    .body {
-      flex: 1;
-    }
-
-    .extra {
-      width: auto;
-      margin-left: 15px;
-    }
-  }
-
-  .config-body {
-    width: 100%;
-    height: auto;
-    float: left;
-    box-sizing: border-box;
-    background-color: white;
-    padding-top: 30px;
-    padding-bottom: 30px;
-
-    .config-item {
-      width: 100%;
-      height: auto;
-      float: left;
-      box-sizing: border-box;
-      padding: 10px 30px;
-
-      .label-name {
-        width: 100%;
-        height: auto;
-        float: left;
-        font-size: 14px;
-        font-weight: normal;
-        color: rgba(0, 0, 0, 0.8);
-        margin-bottom: 10px;
-      }
-
-      .input-box {
-        width: 100%;
-        height: auto;
-        float: left;
-        margin-bottom: 15px;
-      }
-
-      .helper-box {
-        width: 100%;
-        height: auto;
-        float: left;
-        font-size: 12px;
-        color: rgba(0, 0, 0, 0.3);
-        font-weight: normal;
-      }
-    }
-  }
+ 
 }
 </style>
