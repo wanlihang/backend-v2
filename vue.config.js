3824d291bf9d79686b4d9af0ed5a54d167b128d6
0xtyz <616896861@qq.com>
2021-11-24 16:45:29 +0800

    优化url读取

--- vue.config.js
+++ vue.config.js
@@ -7,17 +7,17 @@ module.exports = {
     assetsDir: 'static',
     devServer: {
         proxy: {
-            '/backend':{
-                target:'http://192.168.3.8:8000',
-                changeOrigin:true,
-                ws:true
+            '/backend': {
+                target: 'http://192.168.3.8:8000',
+                changeOrigin: true,
+                ws: true
             }
         }
     },
     pluginOptions: {
         'style-resources-loader': {
-          preProcessor: 'less',
-          patterns: [pathroot.resolve(__dirname, "src/assets/common/variable.less")] // 引入全局样式变量
+            preProcessor: 'less',
+            patterns: [pathroot.resolve(__dirname, "src/assets/common/variable.less")] // 引入全局样式变量
         }
-  },
+    },
 }
