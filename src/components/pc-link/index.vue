12ad118d3c1f20f02e0ecbe5d5ca9c4557771a28
禺狨 <18119604035@163.com>
2021-12-31 14:14:00 +0800

    积分商城链接添加

--- src/components/pc-link/index.vue
+++ src/components/pc-link/index.vue
@@ -139,13 +139,6 @@ export default {
           active: null,
         });
       }
-      if (this.enabledAddons["Credit1Mall"]) {
-        links.push({
-          name: "积分商城",
-          url: "/member/credit1_records",
-          active: null,
-        });
-      }
 
       return links;
     },
