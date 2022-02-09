6de9e208898f991cd9818d7a3c7daa09bd4381c5
禺狨 <18119604035@163.com>
2022-02-09 11:31:38 +0800

    后台mavon编辑器增加视频插入

--- src/components/md-editor.vue
+++ src/components/md-editor.vue
@@ -10,9 +10,24 @@
       :toolbars="toolbars"
       :placeholder="placeholder"
       :autofocus="false"
+      :xssOptions="{
+        whiteList: {
+          iframe: ['src', 'height', 'width'],
+          div: ['style', 'height', 'width', 'align'],
+        },
+      }"
       @change="change"
     >
       <template slot="left-toolbar-after">
+        <el-button
+          type="text"
+          @click="dialogFormVisible = true"
+          aria-hidden="true"
+          class="op-icon fa"
+          title="插入视频资源"
+        >
+          <i class="el-icon-video-camera-solid" />
+        </el-button>
         <button
           type="button"
           @click="showUploadImage = true"
@@ -20,6 +35,45 @@
         ></button>
       </template>
     </mavon-editor>
+    <!-- 插入视频链接的dialog提示框，表单对话框 -->
+    <el-dialog title="插入视频资源" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item
+          style="margin-bottom: 22px"
+          label="视频链接"
+          :label-width="formLabelWidth"
+        >
+          <el-input v-model="form.link" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="链接类型" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="请选择链接类型">
+            <el-option label="iframe标签" value="iframe"></el-option>
+            <el-option label="url链接" value="url"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <!--单机确定按钮后触发 videoLink事件函数，开始格式化链接格式并插入到文本域-->
+        <el-button type="primary" @click="videoLink">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 错误提示框 -->
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      id="link-error"
+    >
+      <span>视频链接格式错误，请重新确认后再输入！</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
 
     <select-image
       :show="showUploadImage"
@@ -68,11 +122,69 @@ export default {
         preview: true, // 预览
       },
       imgCount: 0,
+      dialogFormVisible: false, // 用于控制表单对话框的开启和关闭
+      dialogVisible: false, // 用于控制错误提示对话框的开启和关闭
+      formLabelWidth: "120px", // 设定表单对话框内表单是宽度
+      form: {
+        // 表单对话框内表单的数据
+        link: "",
+        region: "",
+      },
     };
   },
   methods: {
     change(pureContent, renderContent) {
       this.$emit("change", pureContent, renderContent);
+    },
+    videoLink() {
+      // 准备链接模板
+      let linkFrame = "";
+      let linkFrameStart =
+        "<div align=center width=100%><iframe height=500 width=80% src='";
+      let linkFrameEnd = "' frameborder=0 'allowfullscreen'></iframe></div>";
+      // 创建一个div盒子，为提取src做准备
+      var box = document.createElement("div");
+      // 将原始链接插入到盒子中
+      box.innerHTML = this.form.link;
+      // 判断不同的视频原链接类型
+      if (this.form.region == "url") {
+        linkFrame = linkFrameStart + this.form.link + linkFrameEnd;
+      } else if (
+        this.form.region == "iframe" &&
+        box.getElementsByTagName("iframe").length > 0
+      ) {
+        // 从iframe标签中提取src属性
+        linkFrame =
+          linkFrameStart +
+          box.getElementsByTagName("iframe")[0].getAttribute("src") +
+          linkFrameEnd;
+      } else {
+        // 原始链接格式错误时弹出错误提示
+        this.dialogFormVisible = false;
+        this.dialogVisible = true;
+      }
+      // 复原表单文本框内容
+      this.form.link = "";
+      this.form.region = "";
+
+      // 获取文本域中当前光标起始位置、结束位置以及滚动条位置（滚动条位置我认为没有必要，如有需要可以自己取消注释）
+      let textarea = document.getElementsByClassName("auto-textarea-input")[0];
+      let posStart = textarea.selectionStart;
+      let posEnd = textarea.selectionEnd;
+      // let posScroll = document.getElementsByClassName("v-note-edit")[0].scrollTop;
+      // 获取文本域中未选中的的前半部分和后半部分，以被选中内容起始和结束位置做分割点
+      let subStart = this.$refs["mavonEditor"].d_value.substring(0, posStart);
+      let subEnd = this.$refs["mavonEditor"].d_value.substring(
+        posEnd,
+        this.$refs["mavonEditor"].d_value.length
+      );
+      // 拼接并替换文本域内容
+      this.$refs["mavonEditor"].d_value =
+        subStart + "\n" + linkFrame + "\n" + subEnd;
+      // document.getElementsByClassName("v-note-edit")[0].scrollTop = posScroll;
+
+      // 关闭对话框
+      this.dialogFormVisible = false;
     },
     uploadImage(src) {
       this.imgCount++;
