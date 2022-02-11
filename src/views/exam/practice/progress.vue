e88392659ad1b528dd39f60bc7942368e3f43a13
禺狨 <18119604035@163.com>
2022-02-11 10:13:39 +0800

    练习所有列表样式优化

--- src/views/exam/practice/progress.vue
+++ src/views/exam/practice/progress.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="meedu-main-body">
     <back-bar class="mb-30" title="练习进度"></back-bar>
-
     <div class="float-left" v-loading="loading">
       <div class="float-left">
-        <el-table :data="results" class="float-left">
+        <el-table
+          :header-cell-style="{ background: '#f1f2f9' }"
+          :data="results"
+          class="float-left"
+        >
           <el-table-column prop="chapter_name" label="章节"> </el-table-column>
           <el-table-column label="题目数" width="150">
             <template slot-scope="scope">
--- src/views/exam/practice/user.vue
+++ src/views/exam/practice/user.vue
@@ -6,12 +6,17 @@
         text="添加用户"
         p="addons.Paper.practice.user.insert"
         @click="showUserAddWin = true"
+        type="primary"
       ></p-button>
       <el-button @click="exportXlsx" type="primary">导出记录</el-button>
     </div>
     <div class="float-left" v-loading="loading">
       <div class="float-left">
-        <el-table :data="results" class="float-left">
+        <el-table
+          :header-cell-style="{ background: '#f1f2f9' }"
+          :data="results"
+          class="float-left"
+        >
           <el-table-column prop="user_id" label="用户ID" width="120">
           </el-table-column>
           <el-table-column label="用户" width="300">
@@ -50,22 +55,22 @@
           <el-table-column fixed="right" label="操作" width="100">
             <template slot-scope="scope">
               <p-link
-                text="删除"
-                p="addons.Paper.practice.user.delete"
-                type="danger"
-                @click="destroy(scope.row.user_id)"
-              ></p-link>
-              <p-link
                 text="详细"
                 p="addons.Paper.practice.user.progress"
                 type="primary"
-                class="ml-5"
                 @click="
                   $router.push({
                     name: 'PracticeProgress',
                     query: { pid: scope.row.user_id, id: pagination.id },
                   })
                 "
+              ></p-link>
+              <p-link
+                text="删除"
+                class="ml-5"
+                p="addons.Paper.practice.user.delete"
+                type="danger"
+                @click="destroy(scope.row.user_id)"
               ></p-link>
             </template>
           </el-table-column>
