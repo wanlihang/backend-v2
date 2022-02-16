62d3acd07eb1315af6e1a4df8ce119566d26db05
禺狨 <18119604035@163.com>
2022-02-16 14:52:52 +0800

    团购课程团列表筛选状态栏

--- src/views/tuangou/goods/tuanorder.vue
+++ src/views/tuangou/goods/tuanorder.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="meedu-main-body">
     <back-bar class="mb-30" title="团列表"></back-bar>
+    <div class="float-left" v-if="countMap">
+      <el-tabs v-model="filter.status">
+        <el-tab-pane
+          :label="
+            item.name +
+            '(' +
+            (item.key === '-1' ? orderTotal : countMap[item.key]) +
+            ')'
+          "
+          :name="item.key"
+          v-for="(item, index) in filterData.statusList"
+          :key="index"
+        ></el-tab-pane>
+      </el-tabs>
+    </div>
     <div class="float-left" v-loading="loading">
       <div class="float-left">
         <el-table
@@ -74,17 +89,62 @@ export default {
         page: 1,
         size: 10,
       },
+      filter: {
+        status: "-1",
+      },
       total: 0,
       loading: false,
       results: [],
+      countMap: null,
+      filterData: {
+        statusList: [
+          {
+            name: "全部",
+            key: "-1",
+          },
+          {
+            name: "未支付",
+            key: "0",
+          },
+          {
+            name: "组团成功",
+            key: "1",
+          },
+          {
+            name: "组团失败",
+            key: "2",
+          },
+          {
+            name: "组团中",
+            key: "3",
+          },
+        ],
+      },
     };
   },
-
+  computed: {
+    orderTotal() {
+      if (this.countMap === null) {
+        return 0;
+      }
+      let total = 0;
+      for (let i = 1; i < this.filterData.statusList.length; i++) {
+        total += this.countMap[this.filterData.statusList[i].key];
+      }
+      return total;
+    },
+  },
+  watch: {
+    "filter.status"() {
+      this.getResults();
+    },
+  },
   mounted() {
     this.getResults();
   },
   methods: {
     paginationReset() {
+      this.filter.status = -1;
       this.pagination.page = 1;
       this.getResults();
     },
@@ -103,10 +163,12 @@ export default {
       this.loading = true;
       let params = {};
       Object.assign(params, this.pagination);
+      Object.assign(params, this.filter);
       this.$api.TuanGou.Tuan.List(this.pagination.id, params).then((res) => {
         this.loading = false;
         this.results = res.data.data.data;
         this.total = res.data.data.total;
+        this.countMap = res.data.countMap;
       });
     },
     handle(item) {
