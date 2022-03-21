7085014590f59cee9cadf2e310376b172ed80eb8
开源MeEdu网校系统 <13675626825@163.com>
2022-03-21 02:34:54 +0000

    !4 秒杀和团购商品列表的图片显示优化 * 秒杀、团购商品列表显示商品封面兼容问题

--- src/components/thumb-bar.vue
+++ src/components/thumb-bar.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="d-flex" style="width: 100%">
-    <div
-      :style="{
-        'background-image': 'url(' + value + ')',
-        width: width + 'px',
-        height: height + 'px',
-        'background-position': 'center center',
-        'background-size': 'cover',
-        'background-repeat': 'no-repeat',
-      }"
-    ></div>
+    <div class="j-flex" style="width: 120px">
+      <div
+        :style="{
+          'background-image': 'url(' + value + ')',
+          width: width + 'px',
+          height: height + 'px',
+          'background-position': 'center center',
+          'background-size': 'cover',
+          'background-repeat': 'no-repeat',
+        }"
+      ></div>
+    </div>
     <div class="ml-10" style="max-width: 250px">
       {{ title }}
     </div>
