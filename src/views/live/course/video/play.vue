f4069fce70421be8d45d7e9303182ffdf64eaabe
禺狨 <18119604035@163.com>
2022-04-20 17:53:09 +0800

    直播间推流提示刷新提示框

--- src/views/live/course/video/play.vue
+++ src/views/live/course/video/play.vue
@@ -599,7 +599,7 @@ export default {
             align-items: center;
             justify-content: center;
             .live-item-tip {
-              background: rgba(#fff, 0.2);
+              background: rgba(#fff, 0.6);
               padding: 15px 10px;
               font-size: 14px;
               font-weight: 400;
