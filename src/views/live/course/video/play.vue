0d56201fc751250b79977692dfec9f4685aaa0ca
禺狨 <18119604035@163.com>
2022-04-21 10:03:54 +0800

    直播未开始时obs推流提示位置调整

--- src/views/live/course/video/play.vue
+++ src/views/live/course/video/play.vue
@@ -21,7 +21,10 @@
         <div class="live-item">
           <div class="live-item-video">
             <div class="alert-mask" v-if="video.status !== 2 && tip">
-              <div class="live-item-tip d-flex">
+              <div
+                class="live-item-tip d-flex"
+                :class="{ more: video.status === 0 }"
+              >
                 OBS推流成功后若此处无画面显示，请稍等后刷新此页
                 <img
                   class="icon ml-10"
@@ -607,6 +610,10 @@ export default {
               line-height: 14px;
               border-radius: 4px;
               z-index: 100;
+              &.more {
+                margin-bottom: 85px;
+              }
+
               .icon {
                 width: 14px;
                 height: 14px;
